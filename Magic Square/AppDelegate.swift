d33204b1bf4c234c948a1b9385d4861c8222b366
Vyacheslav Biryukov <networmx128bit@gmail.com>
2019-03-01 00:26:19 +0300

    Initial Commit

---
+++ Magic Square/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Magic Square
+//
+//  Created by Вячеслав Бирюков on 01.03.19.
+//  Copyright © 2019 Вячеслав Бирюков. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Magic Square/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Magic Square
+//
+//  Created by Вячеслав Бирюков on 01.03.19.
+//  Copyright © 2019 Вячеслав Бирюков. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
